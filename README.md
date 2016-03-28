116cd949d7afdc572971af28ed44375a805445f5
jerome <jerome@pc-rangeable>
2016-03-28 10:48:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Omcplayshuffle
+
+## Presentation
+
+This simple python script that can execute the omxplayer
+on the current files in the current directory in a
+shuffle order.
+
+## Installation
+
+* clone this git directory on your Raspberry Pi
+* on your local directory as root execute the following command
+```python setup.py install```
---
+++ omxplayshuffle/main.py
@@ -0,0 +1,39 @@
+# -*- coding: utf8
+
+import subprocess
+import random
+import logging
+import os
+
+def run_bash(cmd_, cwd=".", out_err=False):
+    p = subprocess.Popen(cmd_, shell=True, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    if stderr != "":
+        print stderr
+    if out_err:
+        return stdout, stderr
+    else:
+        return stdout
+
+#return the list of file in a specified path
+def get_files_list(path):
+    files = []
+    for file in next(os.walk(path))[2]:
+        files.append(file)
+    logging.debug(files)
+    return files
+
+def main():
+    print os.getcwd()
+    files = get_files_list(os.getcwd())
+    random.shuffle(files)
+    for file in files:
+        out, err = run_bash("omxmplayer {0}".format(file))
+        print out
+        print err
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s-%(levelname)s-%(message)s',
+                        level=logging.DEBUG)
+    main()
